5a285a5ce8266f5c408bcddf85fde1faaa86c5d2
luxiaowen <luxiaowen@pinduoduo.com>
2022-09-09 02:51:13 +0800

    初始化项目

---
+++ src/main/java/com/zzw/loveblog/LoveBlogContext.java
@@ -0,0 +1,14 @@
+package com.zzw.loveblog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveBlogContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveBlogContext.class,args);
+    }
+
+}
---
+++ src/main/java/com/zzw/loveblog/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.zzw.loveblog.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello,world";
+    }
+}
